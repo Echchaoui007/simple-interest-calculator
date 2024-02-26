e57f1725d1df3d15aff7a1a099ed28ad559de5c3
Mohammed Echchaoui <m.echchaoui007@gmail.com>
2024-02-27 00:25:36 +0100

    correcting simple-onterest

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
+# This script calculates simple interest given principal,
+# annual rate of interest and time period in years.
 
-# Simple interest calculator
+# Do not use this in production. Sample purpose only.
 
-principal=$1
-rate=$2
-time=$3
+# Author: Echchaoui007 (IBM)
+# Additional Authors:
+# A name
 
-interest=$((principal * rate * time / 100))
-echo "Simple interest: $interest"
+# Input:
+# p, principal amount
+# t, time period in years
+# r, annual rate of interest
+
+# Output:
+# simple interest = p*t*r
+
+echo"Enter the principal:"
+read p
+echo"Enter rate of interest per year:"
+read r
+echo"Enter time period in years:"
+read t
+
+s=`expr $p \* $t \* $r / 100`
+echo"The simple interest is: "
+echo$s
